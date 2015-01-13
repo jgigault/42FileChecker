83cf9dcd95ab02594f2831ace6c216106f6b9618
Jean-michel GIGAULT <helioflash@mac.com>
2015-01-13 20:04:40 +0100

    enhancement: add some tests for 'ft_printf' (beta)

--- includes/ft_printf.sh
+++ includes/ft_printf.sh
@@ -5,7 +5,7 @@ then
 
 source includes/ft_printf_list.sh
 
-declare -a CHK_FT_PRINTF='( "check_author" "auteur" "check_norme" "norminette" "check_ft_printf_makefile" "makefile" "check_ft_printf_forbidden_func" "forbidden functions" "check_ft_printf_basictests s" "basic tests %s (beta)" "check_ft_printf_basictests d" "basic tests %d (beta)" "check_ft_printf_basictests x" "basic tests %x %X (beta)" "check_ft_printf_basictests 0" "basic tests (beta)" "check_ft_printf_moulitest" "moulitest (https://github.com/yyang42/moulitest)" )'
+declare -a CHK_FT_PRINTF='( "check_author" "auteur" "check_norme" "norminette" "check_ft_printf_makefile" "makefile" "check_ft_printf_forbidden_func" "forbidden functions" "check_ft_printf_basictests s" "basic tests %s (beta)" "check_ft_printf_basictests d" "basic tests %d (beta)" "check_ft_printf_basictests u" "basic tests %u (beta)" "check_ft_printf_basictests x" "basic tests %x (beta)" "check_ft_printf_basictests 0" "basic tests (beta)" "check_ft_printf_moulitest" "moulitest (https://github.com/yyang42/moulitest)" )'
 
 declare -a CHK_FT_PRINTF_AUTHORIZED_FUNCS='(write malloc free exit main)'
 
@@ -41,7 +41,8 @@ function check_ft_printf_all
 		"open .myforbiddenfunc" "more info: forbidden functions"\
 		"open .mybasictestss" "more info: basic tests %s (beta)"\
 		"open .mybasictestsd" "more info: basic tests %d (beta)"\
-		"open .mybasictestsx" "more info: basic tests %x %X (beta)"\
+		"open .mybasictestsu" "more info: basic tests %u (beta)"\
+		"open .mybasictestsx" "more info: basic tests %x (beta)"\
 		"open .mybasictests0" "more info: basic tests (beta)"\
 		"open .mymoulitest" "more info: moulitest"\
 		"_"\
