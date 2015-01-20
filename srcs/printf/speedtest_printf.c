63728a951e74b0b59be3cb558235d6d204c899da
Jean-Michel Gigault <jgigault@student.42.fr>
2015-01-20 12:08:41 +0100

    fix: safer 'speed test' for 'ft_printf'

--- srcs/printf/speedtest_printf.c
+++ srcs/printf/speedtest_printf.c
@@ -1,16 +1,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 int main(void)
 {
- long long a = 0;
+ clock_t time;
+ time = clock();
  int i = 2147483647;
  long l = 2147483647;
  long long ll = 9223372036854775807;
  char c = 0;
  intmax_t im = 9223372036854775807;
- while (a++ < ll)
+ while ((double)time/CLOCKS_PER_SEC < 15)
  {
+  time = clock();
   printf("\n");
   printf("%%\n");
   printf("%d\n", 42);
@@ -18,24 +21,10 @@ int main(void)
   printf("%d%d%d\n", 42, 43, 44);
   printf("%ld\n", l);
   printf("%lld\n", ll);
-  printf("%x\n", 505);
-  printf("%X\n", 505);
-  printf("%p\n", &ll);
-  printf("%20.15d\n", 54321);
-  printf("%-10d\n", 3);
-  printf("% d\n", 3);
-  printf("%+d\n", 3);
-  printf("%010d\n", 1);
-  printf("%hhd\n", c);
-  printf("%jd\n", im);
-  printf("%zd\n", (size_t)i);
-  printf("%u\n", i);
-  printf("%o\n", 40);
-  printf("%#08x\n", 42);
-  printf("%x\n", 1000);
-  printf("%#X\n", 1000);
-  printf("%S\n", L"ݗݜशব");
-  printf("%s%s\n", "test", "test");
+  printf("%x %X %p %20.15d\n", 505, 505, &ll, 54321);
+  printf("%-10d % d %+d %010d %hhd\n", 3, 3, 3, 1, c);
+  printf("%jd %zd %u %o %#08x\n", im, (size_t)i, i, 40, 42);
+  printf("%x %#X %S %s%s\n", 1000, 1000, L"ݗݜशব", "test", "test2");
   printf("%s%s%s\n", "test", "test", "test");
   printf("%C\n", 15000);
  }
