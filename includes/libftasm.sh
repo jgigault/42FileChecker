debcfd450f4920f265c7fbd9cf477a1fcee1d2d5
Jean-Michel Gigault <jgigault@student.42.fr>
2015-05-30 01:37:51 +0200

    dev: libftasm

--- includes/libftasm.sh
+++ includes/libftasm.sh
@@ -80,28 +80,12 @@ function check_libftasm_forbidden_func
 		LIBFTH=`find $MYPATH -name libft.h`
 		check_create_tmp_dir
 		echo "#define NULL ((void *)0)\n#include \"$LIBFTH\"\nint main(void) {" > $F
-		echo "ft_putstr(NULL);" >> $F
 		echo "ft_memset(NULL, 0, 0);" >> $F
-		echo "ft_bzero(NULL, 0);" >> $F
 		echo "ft_memcpy(NULL, NULL, 0);" >> $F
-		echo "ft_memccpy(NULL, NULL, 0, 0);" >> $F
-		echo "ft_memmove(NULL, NULL, 0);" >> $F
-		echo "ft_memchr(NULL, 0, 0);" >> $F
-		echo "ft_memcmp(NULL, NULL, 0);" >> $F
+		echo "ft_bzero(NULL, 0);" >> $F
 		echo "ft_strlen(NULL);" >> $F
 		echo "ft_strdup(NULL);" >> $F
-		echo "ft_strcpy(NULL, NULL);" >> $F
-		echo "ft_strncpy(NULL, NULL, 0);" >> $F
 		echo "ft_strcat(NULL, NULL);" >> $F
-		echo "ft_strncat(NULL, NULL, 0);" >> $F
-		echo "ft_strlcat(NULL, NULL, 0);" >> $F
-		echo "ft_strchr(NULL, 0);" >> $F
-		echo "ft_strrchr(NULL, 0);" >> $F
-		echo "ft_strstr(NULL, NULL);" >> $F
-		echo "ft_strnstr(NULL, NULL, 0);" >> $F
-		echo "ft_strcmp(NULL, NULL);" >> $F
-		echo "ft_strncmp(NULL, NULL, 0);" >> $F
-		echo "ft_atoi(NULL);" >> $F
 		echo "ft_isalpha(0);" >> $F
 		echo "ft_isdigit(0);" >> $F
 		echo "ft_isalnum(0);" >> $F
@@ -109,40 +93,8 @@ function check_libftasm_forbidden_func
 		echo "ft_isprint(0);" >> $F
 		echo "ft_toupper(0);" >> $F
 		echo "ft_tolower(0);" >> $F
-		echo "ft_memalloc(0);" >> $F
-		echo "ft_memdel(NULL);" >> $F
-		echo "ft_strnew(0);" >> $F
-		echo "ft_strdel(NULL);" >> $F
-		echo "ft_strclr(NULL);" >> $F
-		echo "ft_striter(NULL, NULL);" >> $F
-		echo "ft_striteri(NULL, NULL);" >> $F
-		echo "ft_strmap(NULL, NULL);" >> $F
-		echo "ft_strmapi(NULL, NULL);" >> $F
-		echo "ft_strequ(NULL, NULL);" >> $F
-		echo "ft_strnequ(NULL, NULL, 0);" >> $F
-		echo "ft_strsub(NULL, 0, 0);" >> $F
-		echo "ft_strjoin(NULL, NULL);" >> $F
-		echo "ft_strtrim(NULL);" >> $F
-		echo "ft_strsplit(NULL, 0);" >> $F
-		echo "ft_itoa(0);" >> $F
-		echo "ft_putchar(0);" >> $F
-		echo "ft_putchar_fd(0, 0);" >> $F
-		echo "ft_putstr(NULL);" >> $F
-		echo "ft_putstr_fd(NULL, 0);" >> $F
-		echo "ft_putendl(NULL);" >> $F
-		echo "ft_putendl_fd(NULL, 0);" >> $F
-		echo "ft_putnbr(0);" >> $F
-		echo "ft_putnbr_fd(0, 0);" >> $F
-		RET0=`check_fileexists LIBFTASM_BONUS | grep 'All files were found'`
-		if [ "$RET0" != "" ]
-		then
-			echo "ft_lstnew(NULL, 0);" >> $F
-			echo "ft_lstdelone(NULL, NULL);" >> $F
-			echo "ft_lstdel(NULL, NULL);" >> $F
-			echo "ft_lstadd(NULL, NULL);" >> $F
-			echo "ft_lstiter(NULL, NULL);" >> $F
-			echo "ft_lstmap(NULL, NULL);" >> $F
-		fi
+		echo "ft_puts(NULL);" >> $F
+		echo "ft_cat(0);" >> $F
 		echo "return (1); }" >> $F
 		cd "$RETURNPATH"/tmp
 		make re -C "$MYPATH" >/dev/null
@@ -206,64 +158,6 @@ function check_libftasm_extra
 		fi
 	fi
 	else printf $C_GREY"  --Not performed--"$C_CLEAR; fi
-}
-
-function check_libftasm_moulitest
-{	if [ "$OPT_NO_MOULITEST" == "0" ]; then
-    check_moulitest "libftasm"
-	else printf $C_GREY"  --Not performed--"$C_CLEAR; fi
-}
-
-function check_libftasm_static
-{	if [ "$OPT_NO_STATICDECLARATIONS" == "0" ]; then
-	local RET0 TOTAL LOGFILENAME
-	LOGFILENAME=.mystatic
-	$CMD_RM -f $LOGFILENAME $LOGFILENAME
-	$CMD_TOUCH $LOGFILENAME $LOGFILENAME
-	RET0=`check_statics "$MYPATH" | tee $LOGFILENAME`
-	TOTAL=`echo "$RET0" | wc -l | sed 's/ //g'`
-	if [ "$RET0" == "" ]
-	then
-		printf $C_GREEN"  All auxiliary functions are declared as static"$C_CLEAR
-	else
-		if [ "$RET0" == "Files not found" ]
-		then
-			printf $C_RED"  Files not found"$C_CLEAR
-		else
-			printf $C_RED"  $TOTAL warning(s)"$C_CLEAR
-		fi
-	fi
-	else printf $C_GREY"  --Not performed--"$C_CLEAR; fi
-}
-
-function check_statics
-{
-	local TOTAL
-	TOTAL=$(ls -1 $1 | sed '/^\.\/\./d' | grep -E \\.\[c\]$ | grep -E ^ft_)
-	if [ "$TOTAL" == "" ]
-	then
-		printf "Files not found"
-	else
-		for i in $(ls -1 $1 | sed '/^\.\/\./d' | grep -E \\.\[c\]$)
-		do
-			FILEN=$i
-			FILEPATH=$1"/"$i
-			awk -v FILEN="$FILEN" -v FILEN2="$FILEN" 'BEGIN \
-    	{ \
-        	OFS = ""
-	        sub(/\.c/, "", FILEN)
-	    } \
-    	$0 ~ /^[a-z_]+[	 ]+\**[a-z_]*\(.*/ \
-	    { \
-    	    gsub (/^[a-z_]*[	 ]+\**/, "")
-        	gsub (/ *\(.*$/, "")
-	        if ($1 != FILEN) \
-    	    { \
-        	    print FILEN2, " (ligne ", NR, ") : ", $0, "() should be declared as static" \
-	        } \
-    	}' $FILEPATH
-		done
-	fi
 }
 
 function check_libftasm
