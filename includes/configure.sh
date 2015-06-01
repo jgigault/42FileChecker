a8ddf912797b2144686dda23cfd934fc58cbcb87
Jean-Michel Gigault <jgigault@student.42.fr>
2015-06-02 00:30:31 +0200

    recoding configure path

--- includes/configure.sh
+++ includes/configure.sh
@@ -12,19 +12,30 @@ then
 		MYPATH=$(get_config "$PROJECTNAME")
 		display_header
 		display_top "$MYPATH" "${PROJECTNAME_DISPLAY}"
+#		check_configure_read $RETURNFUNC $PROJECTNAME $PROJECTNAME_DISPLAY $HOME
+		check_configure_read $RETURNFUNC $PROJECTNAME $PROJECTNAME_DISPLAY ""
+		cd "$RETURNPATH"
+		$RETURNFUNC
+	}
+
+	function check_configure_read
+	{
+		local AB0 AB2 MYPATH RETURNFUNC PROJECTNAME PROJECTNAME_DISPLAY STARTDIR
+		RETURNFUNC=$1
+		PROJECTNAME=$2
+		PROJECTNAME_DISPLAY=$3
+		STARTDIR=$4
 		printf "  Please type the absolute path to your project:\n"$C_WHITE
-		cd "$HOME/"
+		cd "$STARTDIR/"
 		tput cnorm
-		read -p "  $HOME/" -e AB0
+		read -p "  $STARTDIR/" -e AB0
 		tput civis
 		if [ "$AB0" == "" ]
 		then
-			cd "$RETURNPATH"
-			$RETURNFUNC
 			return
 		else
 			AB0=`echo "$AB0" | sed 's/\/$//'`
-			AB2="$HOME/$AB0"
+			AB2="$STARTDIR/$AB0"
 			while [ "$AB0" == "" -o ! -d "$AB2" ]
 			do
 				display_header
@@ -37,22 +48,21 @@ then
 					printf $C_WHITE""
 				fi
 				tput cnorm
-				read -p "  $HOME/" -e AB0
+				read -p "  $STARTDIR/" -e AB0
 				tput civis
 				if [ "$AB0" == "" ]
 				then
-					cd "$RETURNPATH"
-					$RETURNFUNC
 					return
 				fi
 				AB0=`echo "$AB0" | sed 's/\/$//'`
-				AB2="$HOME/$AB0"
+				AB2="$STARTDIR/$AB0"
 			done
 			cd "$RETURNPATH"
 			save_config "$PROJECTNAME" "$AB2"
 			printf $C_CLEAR""
-			$RETURNFUNC
+			return
 		fi
+
 	}
 
 fi
