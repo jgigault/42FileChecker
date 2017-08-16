cfad24970f32e6b5f5644e3157d6e93d97310bd1
Jean-Michel Gigault <jgigault@student.42.fr>
2017-08-16 11:30:03 +0200

    fix NULL not taken into account in ft_printf test (#90 related)

--- srcs/printf/ft_printf_s.c
+++ srcs/printf/ft_printf_s.c
@@ -13,24 +13,23 @@
 #include "../../tmp/printf.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int				main(int argc, char **argv)
 {
 	int			ret;
 
 	ret = -999;
-	if (argc == 4 && argv[1][1] == 'N')
-		ret = ft_printf(argv[2], NULL);
 	if (argc == 4)
-		ret = ft_printf(argv[2], argv[3]);
+		ret = ft_printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]));
 	else if (argc == 5)
-		ret = ft_printf(argv[2], argv[3], argv[4]);
+		ret = ft_printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]));
 	else if (argc == 6)
-		ret = ft_printf(argv[2], argv[3], argv[4], argv[5]);
+		ret = ft_printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]), (strcmp(argv[5], "NULL") == 0 ? NULL : argv[5]));
 	else if (argc == 7)
-		ret = ft_printf(argv[2], argv[3], argv[4], argv[5], argv[6]);
+		ret = ft_printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]), (strcmp(argv[5], "NULL") == 0 ? NULL : argv[5]), (strcmp(argv[6], "NULL") == 0 ? NULL : argv[6]));
 	else if (argc == 8)
-		ret = ft_printf(argv[2], argv[3], argv[4], argv[5], argv[6], argv[7]);
+		ret = ft_printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]), (strcmp(argv[5], "NULL") == 0 ? NULL : argv[5]), (strcmp(argv[6], "NULL") == 0 ? NULL : argv[6]), (strcmp(argv[7], "NULL") == 0 ? NULL : argv[7]));
 	printf("|%d", ret);
 	return (0);
 }
--- srcs/printf/printf_s.c
+++ srcs/printf/printf_s.c
@@ -1,4 +1,3 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -13,25 +12,23 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int				main(int argc, char **argv)
 {
 	int		ret;
-	char	str;
 
 	ret = -999;
-	if (argc == 4 && argv[1][1] == 'N')
-		ret = printf(argv[2], NULL);
 	if (argc == 4)
-		ret = printf(argv[2], argv[3]);
-	if (argc == 5)
-		ret = printf(argv[2], argv[3], argv[4]);
-	if (argc == 6)
-		ret = printf(argv[2], argv[3], argv[4], argv[5]);
-	if (argc == 7)
-		ret = printf(argv[2], argv[3], argv[4], argv[5], argv[6]);
-	if (argc == 8)
-		ret = printf(argv[2], argv[3], argv[4], argv[5], argv[6], argv[7]);
+		ret = printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]));
+	else if (argc == 5)
+		ret = printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]));
+	else if (argc == 6)
+		ret = printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]), (strcmp(argv[5], "NULL") == 0 ? NULL : argv[5]));
+	else if (argc == 7)
+		ret = printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]), (strcmp(argv[5], "NULL") == 0 ? NULL : argv[5]), (strcmp(argv[6], "NULL") == 0 ? NULL : argv[6]));
+	else if (argc == 8)
+		ret = printf(argv[2], (strcmp(argv[3], "NULL") == 0 ? NULL : argv[3]), (strcmp(argv[4], "NULL") == 0 ? NULL : argv[4]), (strcmp(argv[5], "NULL") == 0 ? NULL : argv[5]), (strcmp(argv[6], "NULL") == 0 ? NULL : argv[6]), (strcmp(argv[7], "NULL") == 0 ? NULL : argv[7]));
 	printf("|%d", ret);
 	return (0);
 }
